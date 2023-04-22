a508896ba6469fe54a3ccf3ec94274c46fcdfb61
Yarema Maksym <macsym2@gmail.com>
2023-04-22 14:08:45 +0300

    json (serialization and deserialization)

--- src/main/java/com/xsakon/Main.java
+++ src/main/java/com/xsakon/Main.java
@@ -1,7 +1,55 @@
 package com.xsakon;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.xsakon.model.Address;
+import com.xsakon.model.Car;
+import com.xsakon.model.Person;
+import com.xsakon.utils.Json;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Json jsonUtils = new Json();
+
+        Address address = new Address("Ukraine", "Lviv");
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(1, "Porsche", "911 Carrera Coupe", "Red", 2020, 137_113d));
+        cars.add(new Car(2, "Porsche", "Panamera", "Aquamarine", 2013, 48_719d));
+        Person person1 = new Person(1, "Francis", "Brunn", address, "fbrunn3@gmail.com", cars);
+        Person person2 = new Person(2, "Francis", "Brunn", address, "fbrunn3@gmail.com", cars);
+
+        List<Person> people = new ArrayList<>();
+        people.add(person1);
+        people.add(person2);
+
+
+
+        try {
+
+            jsonUtils.serializePerson(person1);
+            jsonUtils.serializeListOfPeople(people);
+
+            List<Person> deserializedListOfPeople = jsonUtils.deserializeListOfPeople();
+            Person deserializedPerson = jsonUtils.deserializePerson();
+
+
+            System.out.println("deserializedPerson:");
+            System.out.println(deserializedPerson.toString());
+
+            System.out.println("\ndeserializedListOfPeople:");
+            deserializedListOfPeople.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+
     }
 }
---
+++ src/main/java/com/xsakon/utils/Json.java
@@ -0,0 +1,43 @@
+package com.xsakon.utils;
+
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.xsakon.model.Person;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Json {
+
+    public void serializeListOfPeople(List<Person> people) throws IOException {
+        String json = new Gson().toJson(people);
+        OutputStream outputStream = new FileOutputStream("src/main/resources/people.json");
+        IOUtils.write(json, outputStream, StandardCharsets.UTF_8);
+    }
+
+    public void serializePerson(Person person) throws IOException {
+        String json = new Gson().toJson(person);
+        OutputStream outputStream = new FileOutputStream("src/main/resources/person.json");
+        IOUtils.write(json, outputStream, StandardCharsets.UTF_8);
+    }
+
+
+    public List<Person> deserializeListOfPeople() throws IOException{
+        InputStream inputStream = Resources.getResource("people.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Type peopleListType = new TypeToken<ArrayList<Person>>() {}.getType();
+        return new Gson().fromJson(json, peopleListType);
+    }
+
+
+    public Person deserializePerson() throws IOException {
+        InputStream inputStream = Resources.getResource("person.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        return new Gson().fromJson(json, Person.class);
+    }
+}
